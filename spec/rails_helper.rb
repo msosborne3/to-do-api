1ede39dbffef70744e4172e61077ff8e0fc0d481
msosborne3 <msosborne@ymail.com>
2017-04-22 19:22:11 -0500

    Define .json helper for request specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -29,6 +30,8 @@ RSpec.configuration do |config|
       example.run
     end
   end
+
+  config.include RequestSpecHelper, type: :request
 end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
