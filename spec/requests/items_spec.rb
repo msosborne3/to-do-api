f894a69559b6f2d9e00be41a0c9b29c92b104a6f
msosborne3 <msosborne@ymail.com>
2017-04-22 20:34:09 -0500

    Write request specs for get /lists/:list_id/items

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -7,8 +7,21 @@ RSpec.describe 'Items API' do
   let(:list_id) { list.id }
   let(:id) { items.first.id }
 
+  # Tests that get /lists/:list_id/items has the expected response
   describe 'GET /lists/:list_id/items' do
+    before { get "/lists/#{list_id}/items"}
 
+    # It hould respond with json of size 15 since there
+    # are 15 items in the list
+    it 'returns items' do
+      expect(JSON.parse(response.body)).not_to be_empty
+      expect(JSON.parse(response.body).size).to eq(15)
+    end
+
+    # The request should be successful
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
 
   describe 'POST /lists/:list_id/items' do
