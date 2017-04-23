ab5499272ec52f87ad59e3e0e55c2b2fb678fcc6
msosborne3 <msosborne@ymail.com>
2017-04-22 19:55:37 -0500

    Write exception handler module to handle ActiveRecord:RecordNotFound exception

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,25 +1,33 @@
 class ListsController < ApplicationController
+  # Find the correct list before show, update, and destroy
   before_action :set_list, only: [:show, :update, :destroy]
 
+  # GET /lists
   def index
     @lists = List.all
     json_response(@lists)
   end
 
+  # POST /lists
   def create
+    # use create! so that an ActiveRecord::RecordNotFound exception
+    # will be thrown if needed.
     @list = List.create!(list_params)
     json_response(@list, :created)
   end
 
+  # GET /lists/:id
   def show
     json_response(@list)
   end
 
+  # PUT /lists/:id
   def update
     @list.update(list_params)
     head :no_content
   end
 
+  # DELETE /lists/:id
   def destroy
     @list.destroy
     head :no_content
@@ -27,10 +35,12 @@ class ListsController < ApplicationController
 
   private
 
+  # Only allows specific params
   def list_params
     params.permit(:name)
   end
 
+  # Finds the appropriate list
   def set_list
     @list = List.find(params[:id])
   end
