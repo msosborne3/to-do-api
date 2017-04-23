b78534bf2218ab1c053494fd1bb72b04ed72c6d6
msosborne3 <msosborne@ymail.com>
2017-04-22 22:24:31 -0500

    Remove versioning module

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,49 +1,47 @@
-module V1
-  class ListsController < ApplicationController
-    # Find the correct list before show, update, and destroy
-    before_action :set_list, only: [:show, :update, :destroy]
+class ListsController < ApplicationController
+  # Find the correct list before show, update, and destroy
+  before_action :set_list, only: [:show, :update, :destroy]
 
-    # GET /lists
-    def index
-      @lists = List.all
-      json_response(@lists)
-    end
+  # GET /lists
+  def index
+    @lists = List.all
+    json_response(@lists)
+  end
 
-    # POST /lists
-    def create
-      # use create! so that an ActiveRecord::RecordInvalid exception
-      # will be thrown if needed.
-      @list = List.create!(list_params)
-      json_response(@list, :created)
-    end
+  # POST /lists
+  def create
+    # use create! so that an ActiveRecord::RecordInvalid exception
+    # will be thrown if needed.
+    @list = List.create!(list_params)
+    json_response(@list, :created)
+  end
 
-    # GET /lists/:id
-    def show
-      json_response(@list)
-    end
+  # GET /lists/:id
+  def show
+    json_response(@list)
+  end
 
-    # PUT /lists/:id
-    def update
-      @list.update(list_params)
-      head :no_content
-    end
+  # PUT /lists/:id
+  def update
+    @list.update(list_params)
+    head :no_content
+  end
 
-    # DELETE /lists/:id
-    def destroy
-      @list.destroy
-      head :no_content
-    end
+  # DELETE /lists/:id
+  def destroy
+    @list.destroy
+    head :no_content
+  end
 
-    private
+  private
 
-    # Only allows specific params
-    def list_params
-      params.permit(:name)
-    end
+  # Only allows specific params
+  def list_params
+    params.permit(:name)
+  end
 
-    # Finds the appropriate list
-    def set_list
-      @list = List.find(params[:id])
-    end
+  # Finds the appropriate list
+  def set_list
+    @list = List.find(params[:id])
   end
 end
