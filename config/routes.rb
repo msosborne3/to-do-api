771e56a221f58b21ced0cc8f7fec2c514abb7648
msosborne3 <msosborne@ymail.com>
2017-04-22 22:21:15 -0500

    Remove versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  scope module: :v1, constraints: ApiVersion.new('v1', true) do
-    resources :lists do
-      resources :items
-    end
+  resources :lists do
+    resources :items
   end
 end
