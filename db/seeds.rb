92a1f0b6ac000143254d8fde9d634c4a526da2f6
msosborne3 <msosborne@ymail.com>
2017-04-23 15:47:08 -0500

    Add seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+list1 = List.create({name: 'Daily to do'})
+list1.items.create([{text: 'Grocery shop', done: false}, {text: 'Meal prep', done: false}])
+
+list2 = List.create({name: 'Schoolwork'})
+list2.items.create([{text: 'Science hw 1', done: false}, {text: 'Test prep', done: false}])
