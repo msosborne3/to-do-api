874dce51b02d192811e910261c20b4a38ef95610
msosborne3 <msosborne@ymail.com>
2017-04-23 23:23:09 -0500

    Remove TOGGLE_DONE from items reducer

--- client/src/reducers/itemsReducer.js
+++ client/src/reducers/itemsReducer.js
@@ -9,22 +9,6 @@ export default function itemsReducer(state= {
       let item = {id: id, text: action.item.text, done: action.item.done, parentID: action.item.parentID};
       // Return this list of items and adds it to the list already created.
       return {items: state.items.concat(item)};
-    case 'TOGGLE_DONE':
-      // create an array of items
-      let items = state.items.map(item => {
-        // keep the item if it is not the item to be completed
-        if (item.id !== action.item.id) {
-          return item
-        }
-        // if the item needed is found then done is changed from
-        // false to true or true to false.
-        return Object.assign({}, item, {
-          done: !item.done
-        })
-
-      });
-
-      return {items: items}
     default:
       return state;
   }
