4d23be59f244fae9311c31accc18132b7a94947a
msosborne3 <msosborne@ymail.com>
2017-04-25 14:28:48 -0500

    Style form with bootstrap

--- client/src/containers/ItemsNew.js
+++ client/src/containers/ItemsNew.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-//import { browserHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 import { addItem } from '../actions';
+import { Button } from 'react-bootstrap';
 
 class ItemsNew extends Component {
   constructor(props) {
@@ -23,7 +24,7 @@ class ItemsNew extends Component {
   handleSubmit(ev) {
     ev.preventDefault();
     this.props.addItem(this.props.list, this.state);
-    //browserHistory.push('/lists');
+    browserHistory.push(`/lists/${this.props.list.id}`);
     this.setState({
       text: ''
     })
@@ -33,10 +34,11 @@ class ItemsNew extends Component {
     return (
       <div className="new-list">
         <form onSubmit={(event) => this.handleSubmit(event)}>
-          <label>Text: </label>
-          <input type="text" onChange={(event) => this.handleOnTextChange(event)} value={this.state.text} />
-          <br />
-          <input type="submit" value="Create Item" />
+          <div className="form-group">
+            <label>Text: </label>
+            <input type="text" className="form-control" onChange={(event) => this.handleOnTextChange(event)} value={this.state.text} />
+          </div>
+          <Button type="submit">Create Item</Button>
         </form>
       </div>
     );
@@ -44,7 +46,7 @@ class ItemsNew extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const list = state.lists.lists.find( (list) => list.id == ownProps.routeParams.list_id )
+  const list = state.lists.lists.find( (list) => list.id == ownProps.routeParams.id )
   if (list) {
     return {
       list: list
