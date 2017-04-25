85d739e93bceb3deb16843a96e99642e5c7fdb67
msosborne3 <msosborne@ymail.com>
2017-04-25 01:36:20 -0500

    Remove render from App component

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,16 @@ import './App.css';
 import ListPage from './containers/ListPage';
 
 
-const App = props => ({
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to your to do list!</h2>
-        </div>
-        {props.children}
+
+const App = props => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to your to do list!</h2>
       </div>
-    );
-  }
-});
+      {props.children}
+    </div>
+  );
+}
 
 export default App;
